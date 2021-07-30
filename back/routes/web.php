9b1b040b1f37ef6dadc4f719a524af38dd30cb9d
Gonzalo Cavazos <gonzalo.adriancr@gmail.com>
2021-07-30 13:32:30 -0500

    testing routes back

--- back/routes/web.php
+++ back/routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return 'view';
+Route::any('/hello', function () {    
+    return [request()->all(), request()->input('x')];
 });
